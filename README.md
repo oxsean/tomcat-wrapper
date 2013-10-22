58de19a00430a66f218c33ebc5c1ba92d69316b5
sean yang <oxsean@gmail.com>
2013-10-22 10:08:55 +0800

    添加linux版本

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ tomcat-wrapper
 ==============
 
 采用wrapper启动的tomcat,方便安装服务,支持oom自动重启
+
+###linux版本包含
+
+* apache-tomcat-7.0.42
+* wrapper-linux-x86-64-3.5.22
+
+####使用方法
+```
+create.sh [target path] [app name] {app description}
+```
---
+++ linux/create.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+TARGET_PATH=$1
+APP_NAME=$2
+APP_LONG_NAME=$3
+
+if [ $# -ge 2 ]
+then
+    if [ -z "${APP_LONG_NAME}" ]; then
+        APP_LONG_NAME="${APP_NAME} app server"
+    fi
+    if [ ! -d ${TARGET_PATH} ]; then
+       mkdir -p ${TARGET_PATH}
+    else
+        echo "Error,dir ${TARGET_PATH} has bean exist!"
+        exit 1;
+    fi
+    cp -a tpl/* ${TARGET_PATH}
+    cd ${TARGET_PATH}
+    sed -i "s/@app.name@/${APP_NAME}/g" conf/wrapper.conf
+    sed -i "s/@app.name@/${APP_NAME}/g" bin/tomcat
+    sed -i "s/@app.long.name@/${APP_LONG_NAME}/g" bin/tomcat
+else
+    echo "create.sh [target path] [app name] {app description}"
+fi
+
+
+
+
+
+
